a21e68a4d78bbe4923a0d46ee06f3606954c78d4
Dionysios <barmpoutis@gmail.com>
2013-06-09 10:01:57 -0700

    EIGHTS

---
+++ EIGHTS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numCases; scanf("%d",&numCases);
+    unsigned long long output, N;
+    while(numCases--){
+        scanf("%lld",&N);
+        output = 250*(N-1) + 192;
+        printf("%lld\n", output);
+    }
+    return 0;
+}
