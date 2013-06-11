c5831965d7bdb49287998f08df511a0da66558cf
Dionysios <barmpoutis@gmail.com>
2013-06-11 08:17:13 -0700

    NGM

---
+++ NGM.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    size_t N; scanf("%zd",&N);
+    int output = N % 10;
+    if(output != 0){printf("1\n%d\n",output);}
+    else{printf("2\n");}
+    return 0;
+}
