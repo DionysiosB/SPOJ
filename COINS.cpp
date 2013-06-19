16b66c117ce3578406f5250f4df2e68d9f3a05fc
Dionysios <barmpoutis@gmail.com>
2013-06-19 07:26:10 -0700

    COINS

---
+++ COINS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<unsigned long long, unsigned long long> byteValue;
+
+unsigned long long maxExchange(unsigned long long input){
+    if(input == 0){return 0;}
+    unsigned long long output = byteValue[input];
+    if(!output){output =  max(input, maxExchange(input/4) + maxExchange(input/3) + maxExchange(input/2)); byteValue[input] = output;}
+    return output;
+}
+
+
+int main(){
+    unsigned long long coin = 0;
+    while( scanf("%llu\n",&coin) == 1){printf("%llu\n",maxExchange(coin));}
+    return 0;
+}
