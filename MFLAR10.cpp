5a9f0563df7eeecc8c399a1589c563f818e74c1d
Dionysios <barmpoutis@gmail.com>
2013-07-27 00:23:38 -0700

    MFLAR10

---
+++ MFLAR10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string input = "";
+    const char space = ' ';
+    while(1){
+        getline(cin, input);
+        if(input == "" ){continue;}
+        if(input == "*"){break;}
+        
+        size_t index = 0; while(input[index] == space){index++;}
+        char start = tolower(input[index]);
+
+        bool tautogram = 1;
+        while(tautogram){
+            index = input.find(space, index + 1);
+            if(input[index + 1] == ' '){continue;}
+            if(index == string::npos || index >= input.size() - 1){break;}
+            if(tolower(input[index+1]) != start){tautogram = 0;}
+        }
+        tautogram ? printf("Y\n") : printf("N\n");
+    }
+    return 0;
+}
