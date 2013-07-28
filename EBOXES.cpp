ce34584a1c39080e9dfa0f520a26f702ea97e086
Dionysios <barmpoutis@gmail.com>
2013-07-28 00:11:53 -0700

    EBOXES

---
+++ EBOXES.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    size_t numCases; scanf("%zd\n",&numCases);
+    while(numCases--){
+        unsigned long N, K, T, F; scanf("%lu %lu %lu %lu\n",&N, &K, &T, &F);
+        printf("%lu\n",F + (F - N)/(K - 1));
+    }
+    return 0;
+}
