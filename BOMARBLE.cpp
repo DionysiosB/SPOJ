17a360082cedd9688f1ca4dd81aa13feffa808aa
Dionysios <barmpoutis@gmail.com>
2013-07-30 06:15:45 -0700

    BOMARBLE

---
+++ BOMARBLE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    unsigned n;
+    while(1){
+        scanf("%u\n",&n); if(!n){break;}
+        unsigned long output = (n + 1) * (3*n + 2) / 2;
+        printf("%lu\n",output);
+    }
+    return 0;
+}
