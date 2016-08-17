8a0064723f5c617873be62e5d527b6461566ebc3
Dionysios Barmpoutis <barmpoutis@gmail.com>
2016-08-17 05:35:20 -0400

    DANGER

--- DANGER.cpp
+++ DANGER.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+#include <cstdio>
 #include <cmath>
-using namespace std;
 
 unsigned binaryLog(unsigned long x){
     unsigned output = 0;
@@ -13,7 +12,7 @@ int main(){
     while(true){
         string input = ""; getline(cin, input);  if(input == "00e0"){break;}
         unsigned long n = (10*(input[0]-'0') + (input[1] - '0')) * pow(10,input[3]-'0');
-        unsigned long output = 1 + 2 * (n - pow(2.0, binaryLog(n))); //Numbering starts from 1 in the problem, not 0;
+        unsigned long output = 1 + 2 * (n - pow(2.0, binaryLog(n))); //Numbering starts from 1 
         printf("%lu\n",output);
     }
     return 0;
